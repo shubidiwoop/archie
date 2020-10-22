7aa3753ae224b1832facf714e28a17a172ea2ae4
shubidiwoop <60726144+shubidiwoop@users.noreply.github.com>
2020-10-22 22:02:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     apiPrefix: 'https://mainnet-api.maticvigil.com/v0.1/contract/',
-    apiKey: public.env.APIKEY,
+    apiKey: process.env.APIKEY,
     contractAddress: '0xdb17738e190a38f7928c531a42a7b3dd92d6361e',
     // auditContractAddress: '0xf2de5bf15565f3170225d6613a3e547f531f7207',
     erc20minContractAddress: '0x395bb79463d8c022e52791ce09eac6276765328c',
