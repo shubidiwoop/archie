05b4c258551da57477ecc3f8e2a689bb5a1639e0
shubidiwoop <60726144+shubidiwoop@users.noreply.github.com>
2020-10-22 21:04:39 +0530

    Delete key.js

--- Config/key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID  : "581858283154-l1s0glrvcp0m46lhjd97v9pk9859knk2.apps.googleusercontent.com",
-    googleClientSecret : "OTjB8VodQTXSf0S41_MPiQip",
-    mongoURI: "mongodb+srv://roshan:Y6BuFXPv5VnQNGDb@userinfo.0uwql.mongodb.net/test?retryWrites=true&w=majority",
-    cookieKey: "ukcbkadbncds"
-}
